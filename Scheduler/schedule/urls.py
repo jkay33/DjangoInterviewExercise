a668e74ce1bd330f87517607335fab7eb060bade
Jay Kim <kim.jay33@gmail.com>
2019-05-22 20:42:35 -0400

    Adding additional comments.

--- Scheduler/schedule/urls.py
+++ Scheduler/schedule/urls.py
@@ -2,6 +2,8 @@ from rest_framework import routers
 from .api import scheduleViewSet
 
 #defining router and urls
+#DefaultRouter() includes routes for list, create, retrieve, update, and partial update.
+#DefaultRouter() also includes extra route for .json style formats.
 router = routers.DefaultRouter()
 router.register('api/schedule', scheduleViewSet, 'schedule')
 urlpatterns=router.urls
